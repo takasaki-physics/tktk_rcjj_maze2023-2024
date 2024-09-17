ef82f619a984301da4770a64d9e07f3757b3eef6
yosshi430 <162686032+yosshi430@users.noreply.github.com>
2024-09-17 16:23:33 +0900

    yattao

--- new_algorithm.ino
+++ new_algorithm.ino
@@ -1,26 +1,86 @@
-int zahyou[90][90];
+
+int toutatu_zahyou[90][90];
+int kabe_zahyou[90][90];
 int x = 50;
 int y = 50;
 int right_weight = 0;
 int front_weight = 0;
 int left_weight = 0;
 int go_to = 0;
-int i = 1;//1:east 2:north 3:west 4:south
-int 
+int i = 2;//1:east 2:north 3:west 4:south
 
-void reach_count(){
+int right_reached = 0;//
+int left_reached = 0;
+int front_reached = 0;
 
-    return zahyou[][];
+bool right_wall = 0;
+bool front_wall = 0;
+bool left_wall = 0;
+
+void write_down_wall(i){//壁情報の記入
+ switch (i){
+    case 1://east
+        
+        break;
+    
+    case 2://north
+
+        break;
+
+    case 3://west
+
+        break;
+
+    case 4://south
+
+        break;
+    }
 }
 
-void judge(){
-    right_weight = right_reached * 5 + 1;
-    front_weight = front_reached * 5 + 2;
-    left_weight = left_reached * 5 + 3;
 
-    //kabe ga aru baai sarani plus suru
-    
 
+void judge(i){//重みづけによる拡張右手法
+
+    switch (i){//向きによって重みをつける
+        case 1://east
+            right_weight = toutatu_zahyou[x][y+1] * 5 + 1;
+            front_weight = toutatu_zahyou[x+1][y] * 5 + 2;
+            left_weight = toutatu_zahyou[x][y-1] * 5 + 3;
+            break;
+
+        case 2://north
+            right_weight = toutatu_zahyou[x+1][y] * 5 + 1;
+            front_weight = toutatu_zahyou[x][y-1] * 5 + 2;
+            left_weight = toutatu_zahyou[x-1][y] * 5 + 3;
+            break;
+
+        case 3://west
+            right_weight = toutatu_zahyou[x][y-1] * 5 + 1;
+            front_weight = toutatu_zahyou[x-1][y] * 5 + 2;
+            left_weight = toutatu_zahyou[x][y+1] * 5 + 3;
+            break;
+
+        case 4://south
+            right_weight = toutatu_zahyou[x-1][y] * 5 + 1;
+            front_weight = toutatu_zahyou[x][y+1] * 5 + 2;
+            left_weight = toutatu_zahyou[x+1][y] * 5 + 3;
+            break;
+
+    }
+
+    //壁がある場合更にプラスする
+    if (right_wall == 1){
+        right_weight += 99;
+    }
+    if (front_wall == 1){
+        front_weight += 99;
+    }
+    if (left_wall == 1){
+        left_weight += 99;
+    }
+
+
+    //どこへ行くか決める
     if (right_weight < front_weight){
         n = right_weight;
         go_to = 1;
@@ -32,8 +92,8 @@ void judge(){
         n = left_weight;
         go_to = 3;
     }
-    //go_to 1:right 2:front 3:left
-    return go_to
+    
+    return go_to //go_to 1:right 2:front 3:left
 }
 
 
@@ -42,12 +102,13 @@ void judge(){
 void setup(){
     for (int t = 0; t < 90; t++) {
     for (int j = 0; j < 90; j++) {
-        zahyou[t][j] = 0;  //make it to begining 
+        toutatu_zahyou[t][j] = 0;  //make it to begining 
+        kabe_zahyou[t][j] = 0;
         }
     }
 }
 
 void loop(){
-    
+    //壁情報を取得
 
 }
