886dbe1ba170b6fc4d36a8fe3119f6d85a8d0f4b
yosshi430 <162686032+yosshi430@users.noreply.github.com>
2025-03-27 16:15:02 +0900

    頼む～

--- algorithm/UnifiedAlgorithm/src/main.cpp
+++ algorithm/UnifiedAlgorithm/src/main.cpp
@@ -1128,6 +1128,8 @@ int RightWeight = 0;
 int FrontWeight = 0;
 int LeftWeight = 0;
 
+uint8_t GoalX = 50;
+uint8_t GoalY = 50;
 bool start_Gohome = false; //SSD1306に帰還アルゴリズムに入っているかどうかを送信するかの変数
 
 /*帰還用変数*/
@@ -1543,25 +1545,25 @@ void BFS()
     uint8_t a = x;
     uint8_t b = y;
     cost[a][b] = 1;//現在地のコストを1にする
-    Serial.print("%d GotoHome:");
+    Serial.println("GotoHome:");
 
-    while(!(a == 50 && b == 50)){
+    while(!(a == GoalX && b == GoalY)){
 
         reach_time[a][b] = 1;//そのマスを訪問済みにする
-        Serial.print("%d a =");
-        Serial.print(a);
-        Serial.print("%d b =");
-        Serial.print(b);
+        Serial.print("a =");
+        Serial.println(a);
+        Serial.print("b =");
+        Serial.println(b);
 
-        Serial.print("%d kabe_zahyou[a][b] ==");
-        Serial.print(kabe_zahyou[a][b]);
+        Serial.print("kabe_zahyou[a][b] ==");
+        Serial.println(kabe_zahyou[a][b]);
 
-        Serial.print("%d cost[a][b] == ");
-        Serial.print(cost[a][b]);
+        Serial.print("cost[a][b] == ");
+        Serial.println(cost[a][b]);
         
         for(int n = 1; n <= 8; n *= 2){//そのマスの周りのマスのコストを＋１する
             //int result = static_cast<int>(pow(2, n));
-            Serial.print("%d n =");
+            Serial.print("n =");
             Serial.print(n);
 
             if(!(kabe_zahyou[a][b] & n)) {//kabe_zahyou[][]は0000 の4ビットに絶対方向の東8西4南2北1をそれぞれ割り当てる
@@ -1625,8 +1627,8 @@ void BFS()
 
 
     //スタックを使って逆探索
-    a = 50;
-    b = 50;
+    a = GoalX;
+    b = GoalY;
     Direction = North;
 
     /*デバッグ用*/
@@ -1948,7 +1950,8 @@ void setup(){
         toutatu_zahyou[t][j] = 0; 
         }
     }
-    toutatu_zahyou[50][50] = 1;
+    toutatu_zahyou[GoalX][GoalY] = 1;
+    kabe_zahyou[GoalX][GoalY] = 0;
     // 現在の時刻を取得します
     time_t FirstTime;
     time(&FirstTime);  // 現在の時刻を取得して FirstTime に格納
@@ -2032,10 +2035,6 @@ void loop(){
 
     case 1://座標更新と探索
         WriteDownWall();//帰還用の記録
-        
-        
-
-
         /*デバッグ用
         Homecount += 1;
         if(Homecount >= 10){
